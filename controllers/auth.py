cea5bacf0d0ab076dd6fa258911e44054fd489ba
Shahzad <shazee257@gmail.com>
2024-03-21 17:42:53 +0500

    Add python-jose library for jwt and implement createAccessToken function

--- controllers/auth.py
+++ controllers/auth.py
@@ -1,6 +1,7 @@
 from fastapi import HTTPException
 from schemas.common import serializeDict
-from utils.helpers import hashPassword, verifyPassword
+from utils.helpers import hashPassword, verifyPassword, createAccessToken
+from datetime import timedelta
 
 
 class AuthController:
@@ -15,6 +16,13 @@ class AuthController:
         user = serializeDict(db.user.find_one({"email": email}))
         if user:
             if verifyPassword(password, user.get("password")):
-                return user
+                accessToken = createAccessToken(
+                    user.get("name"),
+                    user.get("email"),
+                    str(user.get("_id")),
+                    timedelta(minutes=20),
+                )
+
+                return {"user": user, "accessToken": accessToken}
             raise HTTPException(401, "Invalid password")
         raise HTTPException(404, "User not found")
--- utils/helpers.py
+++ utils/helpers.py
@@ -1,4 +1,10 @@
 from passlib.context import CryptContext
+from datetime import datetime
+from jose import jwt
+
+
+SECRET_KEY = "nCXEmL3K2hVkbS7uqtTFe4v8dfyQjNJ9WawHZxMBc6GrRgsPAD"
+ALGORITHM = "HS256"
 
 pwd_context = CryptContext(schemes=["sha256_crypt", "des_crypt"])
 
@@ -9,3 +15,10 @@ def hashPassword(password):
 
 def verifyPassword(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def createAccessToken(name: str, email: str, id: str, expiresDelta: int = 30):
+    encode = {"id": id, "name": name, "email": email}
+    expires = datetime.utcnow() + expiresDelta
+    encode.update({"exp": expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
